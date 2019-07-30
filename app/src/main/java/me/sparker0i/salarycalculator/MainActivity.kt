eba73f2dd27ae162616524e67a15536153d404a6
Aaditya Menon <aaditya@sparker0i.me>
2019-07-30 22:35:43 +0530

    Added README

--- app/src/main/java/me/sparker0i/salarycalculator/MainActivity.kt
+++ app/src/main/java/me/sparker0i/salarycalculator/MainActivity.kt
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import me.sparker0i.salarycalculator.model.Category
+import me.sparker0i.salarycalculator.recyclerview.CardAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var model: SalaryViewModel
     private lateinit var categoryObserver: Observer<List<Category>>
 
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<CardAdapter.ViewHolder>? = null
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.component_recycler_view)
+        layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = CardAdapter()
+        recyclerView.adapter = adapter
 
         model = ViewModelProviders.of(this).get(SalaryViewModel::class.java)
 
--- app/src/main/java/me/sparker0i/salarycalculator/recyclerview/CardAdapter.kt
+++ app/src/main/java/me/sparker0i/salarycalculator/recyclerview/CardAdapter.kt
@@ -1,2 +1,60 @@
 package me.sparker0i.salarycalculator.recyclerview
 
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import me.sparker0i.salarycalculator.R
+import me.sparker0i.salarycalculator.model.Category
+import me.sparker0i.salarycalculator.model.Expenses
+import me.sparker0i.salarycalculator.model.Salary
+import me.sparker0i.salarycalculator.model.Type
+
+class CardAdapter : RecyclerView.Adapter<CardAdapter.ViewHolder>() {
+
+    private val components = mutableListOf<Category>()
+
+    init {
+        for (i in 1..10) {
+            components.add(Salary("Basic" , 295368.0))
+        }
+        for (i in 1..10) {
+            components.add(Expenses("Insurance" , 100000.0))
+        }
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var componentName: TextView
+        var amount: TextView
+        var layout: RelativeLayout
+
+        init {
+            componentName = itemView.findViewById(R.id.category_card_name)
+            amount = itemView.findViewById(R.id.category_card_amount)
+            layout = itemView.findViewById(R.id.category_card_holder)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.category_card, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (components.get(position).type == Type.NEGATIVE)
+            holder.layout.setBackgroundColor(Color.parseColor("#FF0000"))
+        else if (components.get(position).type == Type.POSITIVE)
+            holder.layout.setBackgroundColor(Color.parseColor("#239123"))
+
+        holder.amount.text = components.get(position).value.toString()
+        holder.componentName.text = components.get(position).name
+    }
+
+    override fun getItemCount(): Int {
+        return components.size
+    }
+}
